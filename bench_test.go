334326b5c7d0ebf405ee2ecee46bc87346477681
Zashk0 <sasho2004@gmail.com>
2026-04-20 23:46:48 +0300

    Add Go test scaffolding

---
+++ bench_test.go
@@ -0,0 +1,11 @@
+package lab
+
+import "testing"
+
+// TestSanity is a placeholder integration test. Future coverage will
+// be added alongside release channels as they stabilize.
+func TestSanity(t *testing.T) {
+	if 1+1 != 2 {
+		t.Fatal("math is broken")
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Zashk0/cicd-artifact-poisoning-lab
+
+go 1.21
